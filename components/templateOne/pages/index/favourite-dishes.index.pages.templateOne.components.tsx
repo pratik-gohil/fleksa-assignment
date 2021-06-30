229b138c8983372857c14efba7d26d1c459c9d0c
codewithtamil <codewithtami@gmail.com>
2021-06-30 23:05:05 +0530

    fixed padding issue on about-us.index.pages.templateOne.components.tsx and remved fav dishes

--- components/templateOne/pages/index/favourite-dishes.index.pages.templateOne.components.tsx
+++ components/templateOne/pages/index/favourite-dishes.index.pages.templateOne.components.tsx
@@ -1,36 +1,36 @@
-import React, { FunctionComponent } from "react";
-import { Col, Container, Row } from "react-grid-system";
-import styled from "styled-components";
-import Image from "next/image";
+import React, { FunctionComponent } from 'react';
+import { Col, Container, Row } from 'react-grid-system';
+import styled from 'styled-components';
+import Image from 'next/image';
 
-import { useAppSelector } from "../../../../redux/hooks.redux";
-import { selectProducts } from "../../../../redux/slices/index.slices.redux";
-import HorizontalList from "../../common/horizontal-list/horizontal-list.templateOne.component";
-import HorizontalListItem, { IResponsive } from "../../common/horizontal-list/horizontal-list-item.templateOne.component";
-import { useTranslation } from "react-i18next";
-import { selectLanguage } from "../../../../redux/slices/configuration.slices.redux";
-import { BREAKPOINTS } from "../../../../constants/grid-system-configuration";
+import { useAppSelector } from '../../../../redux/hooks.redux';
+import { selectProducts } from '../../../../redux/slices/index.slices.redux';
+import HorizontalList from '../../common/horizontal-list/horizontal-list.templateOne.component';
+import HorizontalListItem, { IResponsive } from '../../common/horizontal-list/horizontal-list-item.templateOne.component';
+import { useTranslation } from 'react-i18next';
+import { selectLanguage } from '../../../../redux/slices/configuration.slices.redux';
+import { BREAKPOINTS } from '../../../../constants/grid-system-configuration';
 
 const WrapperSection = styled.section`
-  border-bottom: ${props => props.theme.border};
-  padding: ${props => props.theme.dimen.X4*4}px 0;
-`
+  border-bottom: ${(props) => props.theme.border};
+  padding: ${(props) => props.theme.dimen.X4 * 4}px 0;
+`;
 
 const Title = styled.h2`
   padding: 0;
   text-align: center;
-  margin: 0 0 ${props => props.theme.dimen.X4*2}px 0;
-`
+  margin: 0 0 ${(props) => props.theme.dimen.X4 * 2}px 0;
+`;
 
 const Card = styled.div`
   display: flex;
   flex-direction: column;
-  border: ${props => props.theme.border};
-  border-radius: ${props => props.theme.borderRadius}px;
+  border: ${(props) => props.theme.border};
+  border-radius: ${(props) => props.theme.borderRadius}px;
   overflow: hidden;
   margin: 0 8px;
   height: 100%;
-`
+`;
 
 const ImageContainer = styled.div`
   width: 100%;
@@ -39,27 +39,25 @@ const ImageContainer = styled.div`
   @media (min-width: ${BREAKPOINTS.lg}px) {
     height: 240;
   }
-`
+`;
 
 const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1 1 auto;
   justify-content: space-between;
-`
+`;
 
-const InfoContainerTop = styled.div`
-
-`
+const InfoContainerTop = styled.div``;
 
 const ItemTitle = styled.h2`
-  padding: 0 ${props => props.theme.dimen.X4}px;
+  padding: 0 ${(props) => props.theme.dimen.X4}px;
   font-size: 20px;
-`
+`;
 
 const ItemDescription = styled.p`
-  padding: 0 ${props => props.theme.dimen.X4}px;
-`
+  padding: 0 ${(props) => props.theme.dimen.X4}px;
+`;
 
 const InfoContainerBottom = styled.div`
   display: flex;
@@ -68,28 +66,28 @@ const InfoContainerBottom = styled.div`
   align-items: center;
   p {
     font-weight: 700;
-    margin: ${props => props.theme.dimen.X4}px 0;
+    margin: ${(props) => props.theme.dimen.X4}px 0;
     &:first-child {
-      margin-left: ${props => props.theme.dimen.X4}px;
+      margin-left: ${(props) => props.theme.dimen.X4}px;
     }
     &:last-child {
-      margin-right: ${props => props.theme.dimen.X4}px;
+      margin-right: ${(props) => props.theme.dimen.X4}px;
     }
   }
-`
+`;
 
 const ItemPrice = styled.p`
   font-size: 20px;
-`
+`;
 
 const ItemToProduct = styled.a`
   p {
     display: flex;
     align-items: center;
-    background-color: ${props => props.theme.primaryColor};
-    border-radius: ${props => props.theme.borderRadius}px;
-    padding: ${props => props.theme.dimen.X2}px ${props => props.theme.dimen.X4}px;
-    border: ${props => props.theme.border};
+    background-color: ${(props) => props.theme.primaryColor};
+    border-radius: ${(props) => props.theme.borderRadius}px;
+    padding: ${(props) => props.theme.dimen.X2}px ${(props) => props.theme.dimen.X4}px;
+    border: ${(props) => props.theme.border};
     span {
       font-weight: 900;
       font-size: 22px;
@@ -99,7 +97,7 @@ const ItemToProduct = styled.a`
       line-height: 1;
     }
   }
-`
+`;
 
 const responsive: IResponsive = {
   sm: {
@@ -121,47 +119,52 @@ const responsive: IResponsive = {
   xxl: {
     width: 360,
     height: 480,
-  }
-}
+  },
+};
 
 const IndexPageFavouriteDishes: FunctionComponent = ({}) => {
-  const { t } = useTranslation("page-index")
-  const language = useAppSelector(selectLanguage)
-  const prductsData = useAppSelector(selectProducts)
+  const { t } = useTranslation('page-index');
+  const language = useAppSelector(selectLanguage);
+  const prductsData = useAppSelector(selectProducts);
 
-  return <WrapperSection>
-    <Container>
-      <Row>
-        <Col>
-          <Title>{t("@favourite-dishes-title")}</Title>
-          <HorizontalList>
-            {prductsData.map((product) => {
-              return <HorizontalListItem key={product.id} responsive={responsive}>
-                <Card>
-                  <ImageContainer>
-                    {product.image && <Image src={product.image} layout="fill" loading="lazy" objectFit="cover" />}
-                  </ImageContainer>
-                  <InfoContainer>
-                    <InfoContainerTop>
-                      <ItemTitle>{product.name_json[language]}</ItemTitle>
-                      <ItemDescription>{product.description_json[language]}</ItemDescription>
-                    </InfoContainerTop>
-                    <InfoContainerBottom>
-                      <ItemPrice>€{product.price}</ItemPrice>
-                      <ItemToProduct href="/menu">
-                        <p>{t("@to-product")} <span>+</span></p>
-                      </ItemToProduct>
-                    </InfoContainerBottom>
-                  </InfoContainer>
-                </Card>
-              </HorizontalListItem>
-            })}
-          </HorizontalList>
-        </Col>
-      </Row>
-    </Container>
-  </WrapperSection>
+  return (
+    <WrapperSection>
+      <Container>
+        <Row>
+          <Col>
+            <Title>{t('@favourite-dishes-title')}</Title>
 
-}
+            <HorizontalList>
+              {prductsData.map((product) => {
+                return (
+                  <HorizontalListItem key={product.id} responsive={responsive}>
+                    <Card>
+                      <ImageContainer>{product.image && <Image src={product.image} layout="fill" loading="lazy" objectFit="cover" />}</ImageContainer>
+                      <InfoContainer>
+                        <InfoContainerTop>
+                          <ItemTitle>{product.name_json[language]}</ItemTitle>
+                          <ItemDescription>{product.description_json[language]}</ItemDescription>
+                        </InfoContainerTop>
 
-export default IndexPageFavouriteDishes
+                        <InfoContainerBottom>
+                          <ItemPrice>€{product.price}</ItemPrice>
+                          <ItemToProduct href="/menu">
+                            <p>
+                              {t('@to-product')} <span>+</span>
+                            </p>
+                          </ItemToProduct>
+                        </InfoContainerBottom>
+                      </InfoContainer>
+                    </Card>
+                  </HorizontalListItem>
+                );
+              })}
+            </HorizontalList>
+          </Col>
+        </Row>
+      </Container>
+    </WrapperSection>
+  );
+};
+
+export default IndexPageFavouriteDishes;
--- templates/one/index.one.templates.tsx
+++ templates/one/index.one.templates.tsx
@@ -1,18 +1,18 @@
-import React, { FunctionComponent } from "react";
-import IndexPageFavouriteDishes from "../../components/templateOne/pages/index/favourite-dishes.index.pages.templateOne.components";
-import IndexPageHero from "../../components/templateOne/pages/index/hero.index.pages.templateOne.components";
-import IndexPageCustomerReviews from "../../components/templateOne/pages/index/customer-reviews.index.pages.templateOne.components";
-import IndexPageAboutUs from "../../components/templateOne/pages/index/about-us.index.pages.templateOne.components";
-
+import React, { FunctionComponent } from 'react';
+// import IndexPageFavouriteDishes from "../../components/templateOne/pages/index/favourite-dishes.index.pages.templateOne.components";
+import IndexPageHero from '../../components/templateOne/pages/index/hero.index.pages.templateOne.components';
+import IndexPageCustomerReviews from '../../components/templateOne/pages/index/customer-reviews.index.pages.templateOne.components';
+import IndexPageAboutUs from '../../components/templateOne/pages/index/about-us.index.pages.templateOne.components';
 
 const IndexPageTemplateOne: FunctionComponent = ({}) => {
-  return <>
-    <IndexPageHero />
-    <IndexPageFavouriteDishes />
-    <IndexPageAboutUs />
-    <IndexPageCustomerReviews />
-  </>
+  return (
+    <>
+      <IndexPageHero />
+      {/* <IndexPageFavouriteDishes /> */}
+      <IndexPageAboutUs />
+      <IndexPageCustomerReviews />
+    </>
+  );
+};
 
-}
-
-export default IndexPageTemplateOne
+export default IndexPageTemplateOne;
