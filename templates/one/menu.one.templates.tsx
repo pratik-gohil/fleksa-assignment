5a2ab990786fe7b1872deb90c281ef2bed1a6a7b
Jayant Singh <jayantsinghxyz@gmail.com>
2021-07-04 21:34:04 +0530

    Menu mobile

--- templates/one/menu.one.templates.tsx
+++ templates/one/menu.one.templates.tsx
@@ -5,22 +5,32 @@ import { useAppSelector } from "../../redux/hooks.redux";
 import { useEffect } from "react";
 import { selectSiblings } from "../../redux/slices/index.slices.redux";
 
-const FullHeightColumn = styled.div`
-  display: block;
+const ColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
   height: calc(100vh - ${props => props.theme.navMobile.height}px);
-  width: 50%;
-  float: left;
   @media (min-width: ${BREAKPOINTS.lg}px) {
+    flex-direction: row;
     height: calc(100vh - ${props => props.theme.navDesktop.height}px);
   }
 `
 
-const FullHeightColumnLeft = styled(FullHeightColumn)`
+const FullHeightColumn = styled.div`
+  height: 50%;
+  width: 100%;
+  float: left;
+  @media (min-width: ${BREAKPOINTS.lg}px) {
+    width: 50%;
+    height: 100%;
+  }
+`
 
+const FullHeightColumnLeft = styled(FullHeightColumn)`
+  overflow: auto;
 `
 
 const FullHeightColumnRight = styled(FullHeightColumn)`
-  
+
 `
 
 const List = styled.ul`
@@ -60,70 +70,6 @@ const MenuPageTemplateOne: FunctionComponent = ({}) => {
         position: new google.maps.LatLng(-33.91747, 151.22912),
         type: "info",
       },
-      {
-        position: new google.maps.LatLng(-33.9191, 151.22907),
-        type: "info",
-      },
-      {
-        position: new google.maps.LatLng(-33.91725, 151.23011),
-        type: "info",
-      },
-      {
-        position: new google.maps.LatLng(-33.91872, 151.23089),
-        type: "info",
-      },
-      {
-        position: new google.maps.LatLng(-33.91784, 151.23094),
-        type: "info",
-      },
-      {
-        position: new google.maps.LatLng(-33.91682, 151.23149),
-        type: "info",
-      },
-      {
-        position: new google.maps.LatLng(-33.9179, 151.23463),
-        type: "info",
-      },
-      {
-        position: new google.maps.LatLng(-33.91666, 151.23468),
-        type: "info",
-      },
-      {
-        position: new google.maps.LatLng(-33.916988, 151.23364),
-        type: "info",
-      },
-      {
-        position: new google.maps.LatLng(-33.91662347903106, 151.22879464019775),
-        type: "parking",
-      },
-      {
-        position: new google.maps.LatLng(-33.916365282092855, 151.22937399734496),
-        type: "parking",
-      },
-      {
-        position: new google.maps.LatLng(-33.91665018901448, 151.2282474695587),
-        type: "parking",
-      },
-      {
-        position: new google.maps.LatLng(-33.919543720969806, 151.23112279762267),
-        type: "parking",
-      },
-      {
-        position: new google.maps.LatLng(-33.91608037421864, 151.23288232673644),
-        type: "parking",
-      },
-      {
-        position: new google.maps.LatLng(-33.91851096391805, 151.2344058214569),
-        type: "parking",
-      },
-      {
-        position: new google.maps.LatLng(-33.91818154739766, 151.2346203981781),
-        type: "parking",
-      },
-      {
-        position: new google.maps.LatLng(-33.91727341958453, 151.23348314155578),
-        type: "library",
-      },
     ];
   
     // Create markers.
@@ -140,7 +86,7 @@ const MenuPageTemplateOne: FunctionComponent = ({}) => {
     initMap()
   }, [ ])
 
-  return <>
+  return <ColumnContainer>
     <FullHeightColumnLeft>
       <List>
         {siblingsData.map(sibling => {
@@ -155,7 +101,7 @@ const MenuPageTemplateOne: FunctionComponent = ({}) => {
     <FullHeightColumnRight>
       <MapContainer id="map"></MapContainer>
     </FullHeightColumnRight>
-  </>
+  </ColumnContainer>
 }
 
 export default MenuPageTemplateOne
