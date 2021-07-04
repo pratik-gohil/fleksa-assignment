ee22d855c220f215c50b72fb786c7ffb58620a59
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-07-04 16:31:04 +0530

    added reservation success page

---
+++ pages/reservation-success.tsx
@@ -0,0 +1,29 @@
+import ReservationSuccessPageTemplateOne from '../templates/one/reservation-success.one.templates';
+import IndexStoreWrapper from '../redux/store.redux';
+import TemplateToShow from '../templates/template-to-show.templates';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { getServerSidePropsCommon } from '../utils/page.utils';
+
+const templateList = [ReservationSuccessPageTemplateOne];
+
+export const getServerSideProps = IndexStoreWrapper.getServerSideProps(async (ctx) => {
+  try {
+    const { redirect } = await getServerSidePropsCommon(ctx, true);
+    if (redirect) return redirect;
+
+    return {
+      props: {
+        ...(await serverSideTranslations((ctx as any).locale, ['header', 'footer'])),
+        templateNumber: 0,
+      },
+    };
+  } catch (error) {
+    throw error;
+  }
+});
+
+function ReservationSuccess({ templateNumber }: any) {
+  return <TemplateToShow templateList={templateList} templateNumber={templateNumber} pageContainer={{ showFooter: false }} />;
+}
+
+export default ReservationSuccess;
