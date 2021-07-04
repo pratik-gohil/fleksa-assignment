32a3dc38d7a2ce225303798a3d97374d288ebd2b
Jayant Singh <jayant840084@gmail.com>
2021-07-04 16:17:49 +0530

    Removed unused import

--- redux/store.redux.ts
+++ redux/store.redux.ts
@@ -1,4 +1,4 @@
-import { configureStore, Store } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import { AddressesSlice } from "./slices/addresses.slices.redux";
 import { ConfigurationSlice } from "./slices/configuration.slices.redux";
