d195bd4729853234c7e59bae74d24e96b78a03e5
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-07-06 22:04:02 +0530

    added update name an email section

--- components/templateOne/pages/account/my-account.right-navigator.pages.templateOne.components.tsx
+++ components/templateOne/pages/account/my-account.right-navigator.pages.templateOne.components.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppSelector } from '../../../../redux/hooks.redux';
 import { selectCustomer } from '../../../../redux/slices/user.slices.redux';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import PencilIconPath from '../../../../public/assets/svg/pencil.svg';
 
 const Wrapper = styled.div`
   width: 70%;
+  padding: 3rem 0;
+  position: relative;
+
+  ::before {
+    content: '';
+    width: 2rem;
+    height: 100%;
+    position: absolute;
+    background: ${(p) => p.theme.primaryColor};
+    top: 0;
+    left: 0;
+  }
 `;
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 500px;
-  margin: 0 auto;
-  margin-bottom: 2em;
+  margin: 0.5rem 0 0 8rem;
 `;
 
 const TitleContainer = styled.div`
@@ -24,55 +36,133 @@ const TitleContainer = styled.div`
 const Title = styled.h1`
   color: ${(p) => p.theme.textDarkColor};
   font-size: 1.6rem;
+  font-weight: 550;
+  padding: 0;
+  margin: 0;
+`;
+
+const TextContainer = styled.div`
+  margin: 1rem 0;
+`;
+
+// const ChangeButtonContainer = styled.div``;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const Button = css`
+  padding: 0.8em 1.5em;
+  font-size: 1rem;
   font-weight: 600;
-  padding: 0;
-  margin: 0;
+  outline: none;
+  border: none;
+  color: white;
+  border-radius: 10px;
+  max-width: max-content;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(1.3);
+  }
 `;
-const Value = styled.p`
-  color: ${(p) => p.theme.textLightColor};
-  font-size: 1.3rem;
-  padding: 0;
-  margin: 0;
+const UpdateButton = styled.button`
+  ${Button}
+  background-color: ${(p) => p.theme.textDarkColor};
 `;
-const TextContainer = styled.div``;
+const CancelButton = styled.button`
+  ${Button}
+  background-color: ${(p) => p.theme.primaryColorRed};
+`;
+
+const InputValue = styled.input`
+  outline: none;
+  border: 1px solid #dddddd;
+  border-radius: 0.5rem;
+
+  padding: 0.5rem 1rem;
+  width: 100%;
+  font-size: 1.2rem;
+`;
+
+// const ChangeButton = styled.div`
+//   border-radius: 50%;
+//   width: 40px;
+//   height: 40px;
+//   display: grid;
+//   place-items: center;
+//   border: 1px solid rgba(0, 0, 0, 0.4);
+//   cursor: pointer;
+
+//   &:hover {
+//     background-color: rgba(0, 0, 0, 0.05);
+//   }
+// `;
+
+const IconContainer = styled.div`
+  border-radius: 50%;
+  width: 34px;
+  height: 34px;
+  border: 1px solid #dddddd;
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+`;
+
+const PencilIcon = styled(PencilIconPath)`
+  font-size: 1.2rem;
+  color: ${(p) => p.theme.textDarkColor};
+`;
 
 export const MyAccountRightSection = () => {
   const customerData = useAppSelector(selectCustomer);
+
+  const [email, setEmail] = useState(customerData.email);
+  const [name, setName] = useState(customerData.name);
 
   return (
     <Wrapper>
       <Content>
         <TitleContainer>
           <Title>Name</Title>
-          {/* {update && (
-                  <ChangeButtonContainer>
-                    <ChangeButton onClick={() => setUpdateName(true)}>
-                      <PencilIcon />
-                    </ChangeButton>
-                  </ChangeButtonContainer>
-                )} */}
+          <IconContainer>
+            <PencilIcon />
+          </IconContainer>
         </TitleContainer>
         <TextContainer>
-          <Value>{customerData.name}</Value>
+          <InputValue type="text" value={name} onChange={(e) => setName(e.target.value)} />
         </TextContainer>
       </Content>
 
-      {/* <ul>
-        <li>
-          <h3>Name</h3>
-          <p>{customerData.name}</p>
-        </li>
-        <li>
-          <h3>Email</h3>
-          <p>{customerData.email || 'N/A'}</p>
-        </li>
-        <li>
-          <h3>Phone Number</h3>
-          <p>
-            +{customerData.country_code} {customerData.phone}
-          </p>
-        </li>
-      </ul> */}
+      <Content>
+        <TitleContainer>
+          <Title>Email</Title>
+          <IconContainer>
+            <PencilIcon />
+          </IconContainer>
+        </TitleContainer>
+        <TextContainer>
+          <InputValue type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </TextContainer>
+      </Content>
+
+      <Content>
+        <TitleContainer>
+          <Title>Phone Number</Title>
+        </TitleContainer>
+
+        <TextContainer></TextContainer>
+      </Content>
+
+      <Content>
+        <ButtonContainer>
+          <UpdateButton>Update</UpdateButton>
+          <CancelButton> Cancel </CancelButton>
+        </ButtonContainer>
+      </Content>
     </Wrapper>
   );
 };
--- interfaces/common/customer.common.interfaces.ts
+++ interfaces/common/customer.common.interfaces.ts
@@ -1,8 +1,8 @@
 export interface ICustomer {
-  name: string
-  email: string|null
-  country_code: string|null
-  phone: string|null
-  email_verified: number|null
-  phone_verified: number|null
+  name: string;
+  email?: string;
+  country_code?: string;
+  phone?: string;
+  email_verified: boolean;
+  phone_verified: boolean;
 }
