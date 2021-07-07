ca6be02e69c48d86b38360e2156223fe220aa5a1
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-07-07 19:26:15 +0530

    added order history page

--- components/templateOne/pages/account/my-account.order-history.pages.templateOne.components.tsx
+++ components/templateOne/pages/account/my-account.order-history.pages.templateOne.components.tsx
@@ -4,16 +4,26 @@ import { useAppSelector } from '../../../../redux/hooks.redux';
 import { selectCustomer } from '../../../../redux/slices/user.slices.redux';
 import { MyAccountOrder } from './partials/my-account.order.partials.account.templateOne.components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  overflow: hidden;
+  height: calc(100vh - ${(p) => p.theme.navDesktop.height}px);
+`;
+
+const InnerWrapper = styled.div`
+  overflow-x: hidden;
+  height: 100%;
+`;
 
 export const MyAccountAllOrderHistory = () => {
   const customerData = useAppSelector(selectCustomer);
 
   return (
     <Wrapper>
-      {customerData.orders?.map((order) => (
-        <MyAccountOrder key={order.id} order={order} />
-      ))}
+      <InnerWrapper>
+        {customerData.orders?.map((order) => (
+          <MyAccountOrder key={order.id} order={order} />
+        ))}
+      </InnerWrapper>
     </Wrapper>
   );
 };
--- components/templateOne/pages/account/partials/my-account.order.partials.account.templateOne.components.tsx
+++ components/templateOne/pages/account/partials/my-account.order.partials.account.templateOne.components.tsx
@@ -4,11 +4,9 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   max-width: 500px;
-  margin: 0 auto;
-  margin-bottom: 2em;
+  margin: 2rem auto;
   box-shadow: 1px 0px 5px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
-  overflow: hidden;
 
   @media (min-width: 850px) {
     max-width: 700px;
@@ -41,6 +39,8 @@ const Type = styled.p`
   font-size: 1rem;
   font-weight: 500;
   margin-bottom: 0.4em;
+  padding: 0;
+  margin: 0;
 `;
 const Quantity = styled(Type)``;
 // const Status = styled(Type)``;
@@ -51,6 +51,8 @@ const Price = styled.h2`
   color: ${(p) => p.theme.primaryColor};
   font-size: 2rem;
   font-size: 700;
+  padding: 0;
+  margin: 0;
 `;
 const TimeDate = styled(Type)``;
 const Dot = styled.span`
@@ -103,12 +105,6 @@ const Receipt = styled.a<{ back: string }>`
   border-bottom-right-radius: 10px;
 `;
 
-// const InfoIcon = styled(InfoCirclepath)`
-//   color: ${(p) => (p.color === 'white' ? 'white' : p.theme.textDarkColor)};
-//   margin-right: 0.5em;
-//   font-size: 1.2rem;
-// `;
-
 interface IMyAccountOrderProps {
   order: IParticularOrder;
 }
@@ -117,39 +113,33 @@ export const MyAccountOrder: FunctionComponent<IMyAccountOrderProps> = ({ order 
   const customDate = new Date(`${order.created_at}`);
 
   return (
-    <>
-      <Container>
-        <TextContainer>
-          <Left>
-            <OrderId href={`/order/${order.id}`}>Order #{order.id}</OrderId>
-            <Type>{order.order_type}</Type>
-            <Quantity>{order.no_of_products} items</Quantity>
-            {/* <Status>{order.payment_status ? order.payment_status :  'UNPAID' }</Status> */}
-          </Left>
-          <Right>
-            <Price>&euro; {order.amount.toFixed(2).replace('.', ',')}</Price>
-            <TimeDate>
-              {customDate.toLocaleString('default', {
-                hour12: false,
-                timeStyle: 'short',
-              })}
-              <Dot>.</Dot>
-              {customDate.toLocaleString('default', { dateStyle: 'medium' })}
-            </TimeDate>
-          </Right>
-        </TextContainer>
+    <Container>
+      <TextContainer>
+        <Left>
+          <OrderId href={`/order/${order.id}`}>Order #{order.id}</OrderId>
+          <Type>{order.order_type}</Type>
+          <Quantity>{order.no_of_products} items</Quantity>
+          {/* <Status>{order.payment_status ? order.payment_status :  'UNPAID' }</Status> */}
+        </Left>
+        <Right>
+          <Price>&euro; {order.amount.toFixed(2).replace('.', ',')}</Price>
+          <TimeDate>
+            {customDate.toLocaleString('default', {
+              hour12: false,
+              timeStyle: 'short',
+            })}
+            <Dot>.</Dot>
+            {customDate.toLocaleString('default', { dateStyle: 'medium' })}
+          </TimeDate>
+        </Right>
+      </TextContainer>
 
-        <ButtonContainer>
-          <Button href={`/order/${order.id}`}>
-            {/* <InfoIcon color="dark" /> */}
-            Review Now
-          </Button>
-          <Receipt target="_blank" rel="noopener noreferrer" back="fill" href={order.pdf_url}>
-            {/* <InfoIcon color="white" /> */}
-            View Receipt
-          </Receipt>
-        </ButtonContainer>
-      </Container>
-    </>
+      <ButtonContainer>
+        <Button href={`/order/${order.id}`}>Review Now</Button>
+        <Receipt target="_blank" rel="noopener noreferrer" back="fill" href={order.pdf_url}>
+          View Receipt
+        </Receipt>
+      </ButtonContainer>
+    </Container>
   );
 };
