51c04e56e7e5bc5040bce4a593fdbca3841d6eee
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-07-07 19:41:04 +0530

    added order view by id request file

---
+++ http/nodeapi/account/get.order-view-by-id.nodeapi.http.ts
@@ -0,0 +1,19 @@
+import {
+  INodeApiHttpGetUserParticularOrderRequestData,
+  INodeApiHttpGetUserParticularOrderResponse,
+} from '../../../interfaces/http/nodeapi/account/get.order-view-by-id.nodeapi.http';
+import { ApiHttpCommon } from '../../base.http';
+import NetNodeApi from '../net.nodeapi.http';
+
+export default class NodeApiHttpGetUserParticularOrder extends ApiHttpCommon {
+  public async get({ order_id }: INodeApiHttpGetUserParticularOrderRequestData) {
+    try {
+      const response = await new NetNodeApi(this.configuration, this.bearerToken).get<INodeApiHttpGetUserParticularOrderResponse>({
+        path: `order/view/${order_id}`,
+      });
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
