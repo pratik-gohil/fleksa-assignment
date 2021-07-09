1133596d3a92c295e9ac2f5b27f7982a69064d80
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-07-09 17:49:11 +0530

    added http files for create address

---
+++ http/nodeapi/account/post.create-address.nodeapi.http.ts
@@ -0,0 +1,32 @@
+import {
+  INodeApiHttpPostCreateNewAddressRequestData,
+  INodeApiHttpPostCreateNewAddressResponseData,
+} from '../../../interfaces/http/nodeapi/account/post.create-address.nodeapi.http';
+import { ApiHttpCommon } from '../../base.http';
+import NetNodeApi from '../net.nodeapi.http';
+
+export default class NodeApiHttpPostCreateNewAddressRequest extends ApiHttpCommon {
+  public async post({ floor, address, address_type, city, country, postal_code, proximity, state }: INodeApiHttpPostCreateNewAddressRequestData) {
+    try {
+      const response = await new NetNodeApi(this.configuration, this.bearerToken).post<INodeApiHttpPostCreateNewAddressResponseData>({
+        path: 'customer/create/address',
+        body: {
+          floor: floor || '',
+          address,
+          address_type,
+          city,
+          country,
+          postal_code,
+          proximity: proximity || '',
+          state: state || '',
+        },
+      });
+      return response;
+    } catch (e) {
+      console.error(e);
+      return {
+        result: false,
+      };
+    }
+  }
+}
---
+++ interfaces/http/nodeapi/account/post.create-address.nodeapi.http.ts
@@ -0,0 +1,17 @@
+type Allowed_address_type = 'OTHER' | 'HOME' | 'WORK';
+
+export interface INodeApiHttpPostCreateNewAddressRequestData {
+  floor?: string;
+  address: string;
+  city: string;
+  state?: string;
+  country: string;
+  postal_code: string;
+  address_type: Allowed_address_type;
+  proximity?: string;
+}
+
+export interface INodeApiHttpPostCreateNewAddressResponseData {
+  result: boolean;
+  message?: string;
+}
