fe24415741c7175d36e8eaa4b87eb08161d18881
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-07-10 22:19:39 +0530

    Added offer page with simple moving carousel

--- templates/one/offers.one.templates.tsx
+++ templates/one/offers.one.templates.tsx
@@ -1,17 +1,19 @@
 import { useTranslation } from 'next-i18next';
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, useRef, useState } from 'react';
+import { useEffect } from 'react';
 import { Col, Container, Row } from 'react-grid-system';
 import styled from 'styled-components';
-import { BREAKPOINTS } from '../../constants/grid-system-configuration';
+import { IContent } from '../../interfaces/common/index.common.interfaces';
 import { useAppSelector } from '../../redux/hooks.redux';
 import { selectContents, selectShop } from '../../redux/slices/index.slices.redux';
+import { convertQuillToHtml } from '../../utils/offers.util';
+import SvgPrevious from '../../public/assets/svg/previous.svg';
+import SvgNext from '../../public/assets/svg/next.svg';
+import { BREAKPOINTS } from '../../constants/grid-system-configuration';
 
 const Wrapper = styled.div`
-  height: calc(100vh - ${(props) => props.theme.navMobile.height}px);
-  width: 100%;
-  @media (min-width: ${BREAKPOINTS.lg}px) {
-    height: calc(100vh - ${(props) => props.theme.navDesktop.height}px);
-  }
+  outline: none;
+  border: none;
 `;
 
 const HeroContainer = styled.section`
@@ -50,13 +52,182 @@ const HeroImage = styled.img`
   object-fit: cover;
 `;
 
+const ContentContainer = styled.div`
+  height: 500px;
+  margin-bottom: 1rem;
+  position: relative;
+  padding: 1rem;
+`;
+const Content = styled.div`
+  margin: 1rem;
+  height: 100%;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  width: 80%;
+  border-radius: 0.5rem;
+  margin: auto;
+  display: flex;
+  padding: 2rem;
+  overflow: hidden;
+
+  @media (max-width: ${BREAKPOINTS.sm}px) {
+    flex-direction: column;
+    width: 100%;
+    padding: 0.5rem;
+    margin: 0;
+  }
+`;
+
+const LeftSection = styled.div`
+  width: 50%;
+  height: 100%;
+
+  @media (max-width: ${BREAKPOINTS.sm}px) {
+    width: 100%;
+  }
+`;
+
+const RightSection = styled(LeftSection)`
+  padding: 0 2rem;
+  text-align: justify;
+  overflow-x: hidden;
+
+  @media (max-width: ${BREAKPOINTS.sm}px) {
+    padding: 1rem;
+    overflow: none;
+  }
+`;
+
+const MainTitle = styled.h2`
+  text-align: left;
+  padding: 0;
+  margin: 0;
+`;
+
+const Image = styled.img`
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+`;
+
+const LeftButton = styled.div<{ leftVisible: boolean }>`
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  left: 50px;
+  top: 50%;
+  transform: translateY(-50%);
+  border-radius: 50%;
+  background: #fff;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  cursor: pointer;
+  transition: 0.3s;
+  display: ${(p) => (p.leftVisible ? 'block' : 'none')};
+
+  svg {
+    transition: 0.3s;
+    fill: rgba(0, 0, 0, 0.4);
+  }
+  &:hover svg {
+    fill: rgba(0, 0, 0, 1);
+  }
+
+  @media (max-width: ${BREAKPOINTS.sm}px) {
+    transform: translateY(280%);
+    left: 10px;
+  }
+`;
+
+const RightButton = styled.div<{ rightVisible: boolean }>`
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  right: 50px;
+  border-radius: 50%;
+  background: #fff;
+  top: 50%;
+  transform: translateY(-50%);
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  cursor: pointer;
+  transition: 0.3s;
+  display: ${(p) => (p.rightVisible ? 'block' : 'none')};
+
+  svg {
+    transition: 0.3s;
+    fill: rgba(0, 0, 0, 0.4);
+  }
+  &:hover svg {
+    fill: rgba(0, 0, 0, 1);
+  }
+
+  @media (max-width: ${BREAKPOINTS.sm}px) {
+    transform: translateY(280%);
+    right: 10px;
+  }
+`;
+
 const OffersPageTemplateOne: FunctionComponent = ({}) => {
   const shopData = useAppSelector(selectShop);
   const { t } = useTranslation('header');
   const contents = useAppSelector(selectContents);
 
+  const [leftArrowVisible, setLeftArrowVisible] = useState(false);
+  const [rightArrowVisible, setRightArrowVisible] = useState(contents.length > 1);
+  const [currentContent, setCurrentContent] = useState<IContent | null>(null);
+  let index = useRef(0);
+
+  const handleLeftArrowClick = async () => {
+    if (index.current <= 0) return;
+
+    index.current -= 1;
+
+    while (true) {
+      if (contents[index.current].type_ !== 'PDF') {
+        setCurrentContent(contents[index.current]);
+
+        if (!contents[index.current - 1]) {
+          setLeftArrowVisible(false);
+          setRightArrowVisible(true);
+        }
+
+        return;
+      }
+
+      index.current -= 1;
+    }
+  };
+
+  const handleRightArrowClick = async () => {
+    if (!(contents.length - 1 > index.current)) return;
+
+    index.current += 1;
+
+    while (true) {
+      if (contents[index.current]?.type_ !== 'PDF') {
+        setCurrentContent(contents[index.current]);
+
+        if (!contents[index.current + 1]) {
+          setRightArrowVisible(false);
+          setLeftArrowVisible(true);
+        }
+
+        return;
+      }
+
+      index.current += 1;
+    }
+  };
+
+  const handleKeyPressClick = async (e: any) => {
+    if (e.keyCode === 37) await handleLeftArrowClick();
+    else if (e.keyCode === 39) await handleRightArrowClick();
+  };
+
+  useEffect(() => {
+    if (contents.length) setCurrentContent(contents[index.current]);
+  }, []);
+
   return (
-    <Wrapper>
+    <Wrapper onKeyDown={handleKeyPressClick} tabIndex={-1}>
       <HeroContainer>
         <HeroImage src={shopData?.place} loading="lazy" alt={shopData?.name} />
         <HeroContentContainer>
@@ -70,7 +241,27 @@ const OffersPageTemplateOne: FunctionComponent = ({}) => {
         </HeroContentContainer>
       </HeroContainer>
 
-      {/* <div dangerouslySetInnerHTML={{ __html: html }} /> */}
+      <ContentContainer>
+        <LeftButton onClick={handleLeftArrowClick} leftVisible={leftArrowVisible}>
+          <SvgPrevious />
+        </LeftButton>
+
+        {!!currentContent && (
+          <Content>
+            <LeftSection>
+              <Image src={currentContent.image} alt="currentContent picture" />
+            </LeftSection>
+            <RightSection>
+              <MainTitle>{currentContent.title}</MainTitle>
+              <div dangerouslySetInnerHTML={{ __html: convertQuillToHtml(JSON.parse(currentContent.description)?.ops) }} />
+            </RightSection>
+          </Content>
+        )}
+
+        <RightButton onClick={handleRightArrowClick} rightVisible={rightArrowVisible}>
+          <SvgNext />
+        </RightButton>
+      </ContentContainer>
     </Wrapper>
   );
 };
