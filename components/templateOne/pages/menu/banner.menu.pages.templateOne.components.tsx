d8bae447875066a52a0b8a89a8e08dab9fd0043c
Jayant Singh <jayant840084@gmail.com>
2021-07-11 02:53:46 +0530

    Added coupon in menu

--- components/templateOne/pages/menu/banner.menu.pages.templateOne.components.tsx
+++ components/templateOne/pages/menu/banner.menu.pages.templateOne.components.tsx
@@ -2,15 +2,21 @@ import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import Image from "next/image";
 import { useAppSelector } from "../../../../redux/hooks.redux";
-import { selectShop } from "../../../../redux/slices/index.slices.redux";
+import { selectOffers, selectShop } from "../../../../redux/slices/index.slices.redux";
 import { Col, Container, Row } from "react-grid-system";
 import { selectLanguage } from "../../../../redux/slices/configuration.slices.redux";
 import MenuFeatures from "./feature.menu.pages.templateOne.components";
 
+import SvgTag from "../../../../public/assets/svg/tag.svg"
+import { BREAKPOINTS } from "../../../../constants/grid-system-configuration";
+
 const BannerContainer = styled.section`
-  height: 400px;
+  height: 500px;
   position: relative;
   border-bottom: ${props => props.theme.border};
+  @media (min-width: ${BREAKPOINTS.lg}px) {
+    height: 400px;
+  }
 `
 
 const ContentTop = styled.div`
@@ -31,17 +37,79 @@ const ContentTopContent = styled.div`
 `
 
 const Title = styled.h1`
-  font-size: 36px;
+  font-size: 30px;
   margin: 0;
+  @media (min-width: ${BREAKPOINTS.lg}px) {
+    font-size: 36px;
+  }
 `
 
 const SubTitle = styled.h2`
-  font-size: 24px;
+  font-size: 18px;
+  @media (min-width: ${BREAKPOINTS.lg}px) {
+    font-size: 24px;
+  }
+`
+
+const OffersWrapper = styled.div`
+  position: relative;
+  margin-top: 36px;
+  @media (min-width: ${BREAKPOINTS.lg}px) {
+    margin-top: 0;
+  }
+`
+
+const OffersContainer = styled.div`
+  background: rgba(0, 0, 0, 0.2);
+  border: ${props => props.theme.border};
+  border-color: rgba(255, 255, 255, 0.8);
+  border-radius: ${props => props.theme.borderRadius}px;
+  max-height: 200px;
+  overflow: auto;
+  padding-top: 12px;
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  p {
+    color: #fff;
+    margin: 0;
+  }
+`
+
+const OfferTitle = styled.p`
+  position: absolute;
+  left: -6px;
+  top: -20px;
+  font-weight: 700;
+  margin: 0;
+  padding: 6px 12px;
+  background: #222;
+  border-radius: ${props => props.theme.borderRadius}px;
+`
+
+const Text = styled.p`
+  padding-left: 6px;
+  font-size: 14px;
+`
+
+const OfferItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  svg {
+    fill: #fff;
+    width: 20px;
+    height: 20px;
+    display: block;
+  }
 `
 
 const MenuPageBanner: FunctionComponent = ({}) => {
   const language = useAppSelector(selectLanguage)
   const shopData = useAppSelector(selectShop)
+  const offersData = useAppSelector(selectOffers)
 
   return <BannerContainer>
     {shopData?.cover && <Image src={shopData.cover} loading="eager" layout="fill" objectFit="cover" />}
@@ -53,6 +121,19 @@ const MenuPageBanner: FunctionComponent = ({}) => {
               <Title>{shopData?.name}</Title>
               <SubTitle>{shopData?.category_json[language]}</SubTitle>
               <MenuFeatures />
+            </Col>
+            <Col>
+              <OffersWrapper>
+                <OffersContainer>
+                  <OfferTitle>OFFER</OfferTitle>
+                  {offersData.map(i => {
+                    return <OfferItem key={i.code}>
+                      <SvgTag />
+                      <Text><strong>{i.provided}% OFF - {i.code}</strong> {i.description_json && i.description_json[language]}</Text>
+                    </OfferItem>
+                  })}
+                </OffersContainer>
+              </OffersWrapper>
             </Col>
           </Row>
         </Container>
