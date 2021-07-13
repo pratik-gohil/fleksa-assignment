704599bdc16005d77c84c4ff2a67117dcd863d52
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-07-13 23:26:40 +0530

    added maps on address page only

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,18 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         pathname: ctx.pathname,
         ...initialProps,
@@ -23,9 +22,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 
@@ -33,38 +32,37 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-
           {/* PWA CONFIG START */}
-          <meta name='application-name' content='PWA App' />
-          <meta name='apple-mobile-web-app-capable' content='yes' />
-          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content='PWA App' />
-          <meta name='description' content='Best PWA App in the world' />
-          <meta name='format-detection' content='telephone=no' />
-          <meta name='mobile-web-app-capable' content='yes' />
-          <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
-          <meta name='msapplication-TileColor' content='#2B5797' />
-          <meta name='msapplication-tap-highlight' content='no' />
-          <meta name='theme-color' content='#000000' />
+          <meta name="application-name" content="PWA App" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="PWA App" />
+          <meta name="description" content="Best PWA App in the world" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
 
-          <link rel='apple-touch-icon' href='/static/icons/touch-icon-iphone.png' />
-          <link rel='apple-touch-icon' sizes='152x152' href='/static/icons/touch-icon-ipad.png' />
-          <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/touch-icon-iphone-retina.png' />
-          <link rel='apple-touch-icon' sizes='167x167' href='/static/icons/touch-icon-ipad-retina.png' />
+          <link rel="apple-touch-icon" href="/static/icons/touch-icon-iphone.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/static/icons/touch-icon-ipad.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/touch-icon-iphone-retina.png" />
+          <link rel="apple-touch-icon" sizes="167x167" href="/static/icons/touch-icon-ipad-retina.png" />
 
-          <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
-          <link rel='manifest' href='/static/manifest.json' />
-          <link rel='mask-icon' href='/static/icons/safari-pinned-tab.svg' color='#5bbad5' />
-          <link rel='shortcut icon' href='/favicon.ico' />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+          <link rel="manifest" href="/static/manifest.json" />
+          <link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/favicon.ico" />
 
-          <link rel='apple-touch-startup-image' href='/static/images/apple_splash_2048.png' sizes='2048x2732' />
-          <link rel='apple-touch-startup-image' href='/static/images/apple_splash_1668.png' sizes='1668x2224' />
-          <link rel='apple-touch-startup-image' href='/static/images/apple_splash_1536.png' sizes='1536x2048' />
-          <link rel='apple-touch-startup-image' href='/static/images/apple_splash_1125.png' sizes='1125x2436' />
-          <link rel='apple-touch-startup-image' href='/static/images/apple_splash_1242.png' sizes='1242x2208' />
-          <link rel='apple-touch-startup-image' href='/static/images/apple_splash_750.png' sizes='750x1334' />
-          <link rel='apple-touch-startup-image' href='/static/images/apple_splash_640.png' sizes='640x1136' />
+          <link rel="apple-touch-startup-image" href="/static/images/apple_splash_2048.png" sizes="2048x2732" />
+          <link rel="apple-touch-startup-image" href="/static/images/apple_splash_1668.png" sizes="1668x2224" />
+          <link rel="apple-touch-startup-image" href="/static/images/apple_splash_1536.png" sizes="1536x2048" />
+          <link rel="apple-touch-startup-image" href="/static/images/apple_splash_1125.png" sizes="1125x2436" />
+          <link rel="apple-touch-startup-image" href="/static/images/apple_splash_1242.png" sizes="1242x2208" />
+          <link rel="apple-touch-startup-image" href="/static/images/apple_splash_750.png" sizes="750x1334" />
+          <link rel="apple-touch-startup-image" href="/static/images/apple_splash_640.png" sizes="640x1136" />
 
           {/* PWA CONFIG END */}
 
@@ -72,13 +70,19 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet"></link>
 
-          <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`}></script>
+          {(((this.props as any).pathname as string).startsWith('/menu') ||
+            ((this.props as any).pathname as string) === '/checkout' ||
+            ((this.props as any).pathname as string) === '/account/addresses') && (
+            <script
+              src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`}
+            ></script>
+          )}
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
