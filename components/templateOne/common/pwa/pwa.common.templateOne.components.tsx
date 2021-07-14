30d7e17d47a2c9e14a5d5115d68d72fc5bc42df8
Jayant Singh <jayant840084@gmail.com>
2021-07-14 21:21:24 +0530

    PWA

--- components/templateOne/common/pwa/pwa.common.templateOne.components.tsx
+++ components/templateOne/common/pwa/pwa.common.templateOne.components.tsx
@@ -23,6 +23,7 @@ export const PwaMeta = () => {
     <link rel="apple-touch-icon" sizes="152x152" href="/static/icons/touch-icon-ipad.png" />
     <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/touch-icon-iphone-retina.png" />
     <link rel="apple-touch-icon" sizes="167x167" href="/static/icons/touch-icon-ipad-retina.png" /> */}
+    <link rel="apple-touch-icon" href={shopData?.logo} />
 
     {/* <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" /> */}
     {/* <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" /> */}
