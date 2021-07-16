38933585ba55e587e459aa4d371541c3386bf1cb
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-07-16 18:28:52 +0530

    added ganalytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,6 +37,16 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet"></link>
 
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NDJJ6J9');`,
+            }}
+          ></script>
+
           {(((this.props as any).pathname as string).startsWith('/menu') ||
             ((this.props as any).pathname as string) === '/checkout' ||
             ((this.props as any).pathname as string) === '/account/addresses') && (
