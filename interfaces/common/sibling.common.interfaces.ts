d7da6f355e8698ab023d615b97672f91836d78f4
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-07-18 19:34:41 +0530

    replaced cover of multi carousel

--- interfaces/common/sibling.common.interfaces.ts
+++ interfaces/common/sibling.common.interfaces.ts
@@ -1,34 +1,37 @@
-import { ILanguageData } from "./language-data.common.interfaces";
-import { ITimings } from "./shop.common.interfaces";
+import { ILanguageData } from './language-data.common.interfaces';
+import { ITimings } from './shop.common.interfaces';
 
 export interface ISibling {
   address: {
-    address: string
-    area: string
-    city: string
-    floor: string
-    postal_code: string
-    lat: number
-    lon: number
+    address: string;
+    area: string;
+    city: string;
+    floor: string;
+    postal_code: string;
+    lat: number;
+    lon: number;
     availability: {
       has_delivery: {
-        always: true
-      }
+        always: true;
+      };
       has_dinein: {
-        always: true
-      }
+        always: true;
+      };
       has_pickup: {
-        always: true
-      }
+        always: true;
+      };
       has_reservations: {
-        always: false
-      }
-    }
-  }
-  name: string
-  category_json: ILanguageData
-  id: number
-  logo: string
-  urlpath: string
-  timings: ITimings
+        always: false;
+      };
+    };
+  };
+  name: string;
+  category_json: ILanguageData;
+  id: number;
+  logo: string;
+  urlpath: string;
+  timings: ITimings;
+  cover_json: {
+    images: Array<string>;
+  };
 }
