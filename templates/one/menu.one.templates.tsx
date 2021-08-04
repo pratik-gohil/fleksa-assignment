34d4e2598b9939aed5b77847c7fe910fed399aa1
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-08-04 23:26:57 +0530

    replaced map person and logo icons

--- templates/one/menu.one.templates.tsx
+++ templates/one/menu.one.templates.tsx
@@ -278,8 +278,8 @@ const MenuPageTemplateOne: FunctionComponent = ({}) => {
 
       const marker = new google.maps.Marker({
         position,
-        icon: shopData?.logo && {
-          url: shopData?.logo,
+        icon: {
+          url: '/assets/png/restaurant.png',
           scaledSize: new google.maps.Size(40, 40),
           origin: new google.maps.Point(0, 0),
           anchor: new google.maps.Point(0, 0),
@@ -310,7 +310,7 @@ const MenuPageTemplateOne: FunctionComponent = ({}) => {
 
     currentLocationMarker = new google.maps.Marker({
       icon: {
-        url: '/assets/png/person.png',
+        url: '/assets/png/user.png',
         scaledSize: new google.maps.Size(40, 40),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(0, 0),
@@ -381,7 +381,7 @@ const MenuPageTemplateOne: FunctionComponent = ({}) => {
       const response = await new PyApiHttpPostAddress(configuration).postAll({
         floor: '',
         address: '',
-        addressType: "HOME",
+        addressType: 'HOME',
         city: '',
         area,
         shopId: shopData?.id,
@@ -498,13 +498,17 @@ const MenuPageTemplateOne: FunctionComponent = ({}) => {
 
   useEffect(() => {
     const tempRestaurantsToShow = filterName === 'has_delivery' ? deliveryFilterData : siblingsData.filter((sibling) => sibling.address[filterName]);
+
     setRestaurantsToShow(tempRestaurantsToShow);
+
     Object.keys(markers).map((i) => markers[i].marker.setMap(tempRestaurantsToShow?.find((o) => o.id === Number(i)) ? map : null));
+
     if (selectedId) markers[selectedId].infoWindow.close();
     setSelectedId(null);
   }, [filterName, deliveryFilterData]);
 
   let restaurantListView: ReactNode;
+
   if (restaurantsToShow && restaurantsToShow.length > 0) {
     restaurantListView = restaurantsToShow.map((sibling) => {
       const area = sibling.address?.area ? sibling.address?.area + ' ' : '';
@@ -626,8 +630,9 @@ const MenuPageTemplateOne: FunctionComponent = ({}) => {
           {restaurantListView}
         </List>
       </FullHeightColumnLeft>
+
       <FullHeightColumnRight>
-        <MapContainer id="map"></MapContainer>
+        <MapContainer id="map" />
       </FullHeightColumnRight>
     </ColumnContainer>
   );
