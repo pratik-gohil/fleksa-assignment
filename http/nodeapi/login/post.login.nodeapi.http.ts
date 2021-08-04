4e1dc44e66d81f22a8341395b2e079a3336bdee9
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-08-04 19:11:22 +0530

    Updated request paylaod

--- http/nodeapi/login/post.login.nodeapi.http.ts
+++ http/nodeapi/login/post.login.nodeapi.http.ts
@@ -1,25 +1,22 @@
-import {
-  INodeApiHttpPostLoginRequestData,
-  INodeApiHttpPostLoginResponse
-} from "../../../interfaces/http/nodeapi/login/post.login.nodeapi.http"
-import { ApiHttpCommon } from "../../base.http"
-import NetNodeApi from "../net.nodeapi.http"
+import { INodeApiHttpPostLoginRequestData, INodeApiHttpPostLoginResponse } from '../../../interfaces/http/nodeapi/login/post.login.nodeapi.http';
+import { ApiHttpCommon } from '../../base.http';
+import NetNodeApi from '../net.nodeapi.http';
 
 export default class NodeApiHttpPostLogin extends ApiHttpCommon {
-
-  public async post({ countryCode, phone, shopId }: INodeApiHttpPostLoginRequestData) {
+  public async post({ countryCode, phone, shopId, languageCode }: INodeApiHttpPostLoginRequestData) {
     try {
       const response = await new NetNodeApi(this.configuration).post<INodeApiHttpPostLoginResponse>({
-        path: "role/customer/login",
+        path: 'role/customer/login',
         body: {
           country_code: countryCode,
           phone: phone,
-          shop_id: shopId
-        }
-      })
-      return response
+          shop_id: shopId,
+          languageCode: languageCode || 'de',
+        },
+      });
+      return response;
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 }
