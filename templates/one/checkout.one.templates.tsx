18e38aadeb3834a551bdabd64138276986465808
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-08-15 14:55:38 +0530

    changed pre-order -> order now

--- templates/one/checkout.one.templates.tsx
+++ templates/one/checkout.one.templates.tsx
@@ -13,15 +13,9 @@ import CheckoutPagePromoCode from '../../components/templateOne/pages/checkout/p
 import CheckoutPageSummary from '../../components/templateOne/pages/checkout/summary.checkout.pages.templateOne.components';
 import CheckoutPageTip from '../../components/templateOne/pages/checkout/tip.checkout.pahes.templateOne.components';
 import { BREAKPOINTS } from '../../constants/grid-system-configuration';
-// import { LS_GUEST_USER_ADDRESS } from '../../constants/keys-local-storage.constants';
-// import PyApiHttpPostAddress from '../../http/pyapi/address/post.address.pyapi.http';
-// import { IParticularAddress } from '../../interfaces/common/customer.common.interfaces';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks.redux';
 import { selectPaymentMethod, updatePaymentMethod } from '../../redux/slices/checkout.slices.redux';
-// import { updateError } from '../../redux/slices/common.slices.redux';
-// import { selectConfiguration, selectSelectedMenu } from '../../redux/slices/configuration.slices.redux';
 import { selectShop } from '../../redux/slices/index.slices.redux';
-// import { selectAddressByType, selectBearerToken, updateNewCustomerAddress } from '../../redux/slices/user.slices.redux';
 
 const CartContainerLarge = styled.div`
   display: none;
@@ -39,65 +33,12 @@ const CartContainerSmall = styled.div`
 
 const CheckoutPageTemplateOne: FunctionComponent = ({}) => {
   const dispatch = useAppDispatch();
-  // const configuration = useAppSelector(selectConfiguration);
-  // const bearerToken = useAppSelector(selectBearerToken);
-  // const shopId = useAppSelector(selectSelectedMenu);
+
   const paymentMethod = useAppSelector(selectPaymentMethod);
   const shopData = useAppSelector(selectShop);
-  // const addressByType = useAppSelector((state) => selectAddressByType(state, 'HOME'));
-
-  // async function addGuestAddressOnServerIfExists() {
-  //   // check if guest address exists. If it does add it to server
-  //   const guestAddressString = window.localStorage.getItem(LS_GUEST_USER_ADDRESS);
-  //   if (guestAddressString && bearerToken && shopId) {
-  //     const guestAddress = JSON.parse(guestAddressString) as IGuestAddress;
-  //     const response = await new PyApiHttpPostAddress(configuration).postAll({
-  //       floor: '',
-  //       shopId: shopId,
-  //       address: guestAddress.address,
-  //       addressType: guestAddress.address_type,
-  //       city: guestAddress.city,
-  //       postalCode: guestAddress.postal_code,
-  //       area: '',
-  //       token: bearerToken,
-  //     });
-  //     console.log(response);
-  //     if (response) {
-  //       if (!response.result) {
-  //         dispatch(
-  //           updateError({
-  //             show: true,
-  //             message: response.description,
-  //             severity: 'error',
-  //           }),
-  //         );
-  //         return;
-  //       }
-  //       if (response.customer.is_customer && response.customer.details?.customer_address_id) {
-  //         window.localStorage.removeItem(LS_GUEST_USER_ADDRESS);
-  //         const addressAdded: IParticularAddress = {
-  //           id: response.customer.details.customer_address_id,
-  //           address_type: guestAddress.address_type,
-  //           floor: guestAddress.floor,
-  //           address: guestAddress.address,
-  //           country: '',
-  //           postal_code: guestAddress.postal_code,
-  //           city: guestAddress.city,
-  //           state: '',
-  //         };
-  //         dispatch(updateNewCustomerAddress(addressAdded));
-  //         dispatch(updateSelectedAddressId(response.customer.details.customer_address_id));
-  //       } else {
-  //         dispatch(updateSelectedAddressId(addressByType?.id));
-  //       }
-  //     }
-  //   }
-  // }
 
   useEffect(() => {
     if (window !== 'undefined') {
-      // addGuestAddressOnServerIfExists();
-
       if ((paymentMethod === 'PAYPAL' && !shopData?.paypal_available) || (paymentMethod === 'STRIPE' && !shopData?.stripe_available))
         dispatch(updatePaymentMethod('CASH')); // ? Default method on checkout
     }
