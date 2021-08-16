85600d0f0496a95c6ed2d9df8198fe2626c03267
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-08-16 20:26:17 +0530

    Blocked force view by menu using id in menu page

--- pages/menu/[id].tsx
+++ pages/menu/[id].tsx
@@ -7,7 +7,7 @@ import { updateCategories, updateParts, updateSides } from '../../redux/slices/m
 import PyApiHttpGetMenu from '../../http/pyapi/menu/get.menu.index.pyapi.http';
 import { getServerSidePropsCommon } from '../../utils/page.utils';
 import Cookies from 'cookies';
-import { COOKIE_SELECTED_MENU_ID, COOKIE_SELECTED_MENU_URLPATH } from '../../constants/keys-cookies.constants';
+import { COOKIE_SELECTED_MENU_URLPATH } from '../../constants/keys-cookies.constants';
 import { updateSelectedMenu, updateSelectedMenuUrlpath } from '../../redux/slices/configuration.slices.redux';
 import { updateSiblings } from '../../redux/slices/index.slices.redux';
 import NodeApiHttpGetUserAllAddress from '../../http/nodeapi/account/get.account.all-address.nodeapi.http';
@@ -19,37 +19,58 @@ const templateList = [MenuByIdPageTemplateOne];
 
 export const getServerSideProps = IndexStoreWrapper.getServerSideProps(async (ctx) => {
   try {
+    const shopId = Number(ctx.query.id);
+
+    if (isNaN(shopId)) throw new Error('Shop id not found');
+
     const { redirect, configuration, responseIndex, bearerToken } = await getServerSidePropsCommon(ctx, false);
     if (redirect) return redirect;
 
     const cookies = new Cookies(ctx.req, ctx.res);
-    cookies.set(COOKIE_SELECTED_MENU_ID, ctx.query.id as string, {
-      path: '/',
-      httpOnly: false,
-      maxAge: 365 * 24 * 60 * 60 * 1000,
-      sameSite: 'lax',
-    });
+
     let urlPath: string | undefined;
-    if (responseIndex?.shop.id === Number(ctx.query.id)) {
-      urlPath = responseIndex?.shop.urlpath;
-    } else {
+    let menuId: number | undefined;
+
+    if (responseIndex?.siblings.length) {
       const siblingData = responseIndex?.siblings.find((i) => i.id == Number(ctx.query.id));
       urlPath = siblingData?.urlpath;
+      menuId = siblingData?.id;
+
+      // TODO: If that menu id is not exist redirect it root as default one
+      if (!menuId)
+        return {
+          redirect: {
+            permanent: false,
+            destination: `/menu/${responseIndex?.shop.id}`,
+          },
+        };
+    } else if (Number(ctx.query.id) === responseIndex?.shop.id) {
+      urlPath = responseIndex?.shop.urlpath;
+      menuId = responseIndex?.shop.id;
+    } else {
+      /**
+       * In case customer try to visit different restaurant menu redirect them to correspond restaurant
+       * location.
+       */
+      return {
+        redirect: {
+          permanent: false,
+          destination: `/menu/${responseIndex?.shop.id}`,
+        },
+      };
     }
+
     cookies.set(COOKIE_SELECTED_MENU_URLPATH, urlPath as string, {
       path: '/',
       httpOnly: false,
       maxAge: 365 * 24 * 60 * 60 * 1000,
       sameSite: 'lax',
     });
-    ctx.store.dispatch(updateSelectedMenu(ctx.query.id as string));
+    ctx.store.dispatch(updateSelectedMenu(menuId));
     ctx.store.dispatch(updateSelectedMenuUrlpath(urlPath));
     ctx.store.dispatch(updateSiblings(responseIndex?.siblings));
 
-    const shopId = Number(ctx.query.id);
-
-    if (isNaN(shopId)) throw new Error('Shop id not found');
-    const responseMenu = await new PyApiHttpGetMenu(configuration).get({ shopId });
+    const responseMenu = await new PyApiHttpGetMenu(configuration).get({ shopId: menuId || shopId });
     ctx.store.dispatch(updateCategories(responseMenu?.categories));
     ctx.store.dispatch(updateSides(responseMenu?.sides));
     ctx.store.dispatch(updateParts(responseMenu?.parts));
