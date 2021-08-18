9bce8de22287874700167ca99316ac054f5b4358
MohamedJakkariya <mohamed.jakkariya@fleksa.com>
2021-08-18 09:16:37 -0700

    added tracking for fav dishes

--- components/templateOne/common/amplitude/customLink.tsx
+++ components/templateOne/common/amplitude/customLink.tsx
@@ -5,11 +5,13 @@ import { useAppSelector } from '../../../../redux/hooks.redux';
 import { selectLanguageCode } from '../../../../redux/slices/configuration.slices.redux';
 import { amplitudeEvent, constructEventName } from '../../../../utils/amplitude.util';
 import styled, { DefaultTheme, StyledComponent } from 'styled-components';
+import { IProduct } from '../../../../interfaces/common/product.common.interfaces';
 
 interface ICustomLinkProps {
   amplitude: {
     type: AmplitudeEventNodes;
     text: string;
+    eventProperties?: Record<string, number | string> | IProduct;
   };
   placeholder?: string;
   href: string;
@@ -24,7 +26,7 @@ const CustomLink: FunctionComponent<ICustomLinkProps> = ({ amplitude, children, 
   const handleLinkClick = async (e: React.MouseEvent<HTMLAnchorElement, MouseEvent> | undefined) => {
     e?.preventDefault();
     alert(href);
-    amplitudeEvent(constructEventName(amplitude.text, amplitude.type));
+    amplitudeEvent(constructEventName(amplitude.text, amplitude.type), amplitude.eventProperties);
     router.push(`/${isLanguageChange ? (router.locale === 'en' ? 'de' : 'en') : languageCode}${href}`);
   };
 
