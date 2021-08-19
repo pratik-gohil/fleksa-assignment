7c880b6c94e05b73d1f9a242a52221e64f7f31c2
MohamedJakkariya <mohamed.jakkariya@fleksa.com>
2021-08-19 06:35:19 -0700

    added event for multiple product clicks

--- components/templateOne/pages/menu/multiple-selector.pages.templateOne.components.tsx
+++ components/templateOne/pages/menu/multiple-selector.pages.templateOne.components.tsx
@@ -1,52 +1,95 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useState } from 'react';
 
-import { useAppSelector } from "../../../../redux/hooks.redux";
-import { selectLanguage, selectLanguageCode } from "../../../../redux/slices/configuration.slices.redux";
-import formatCurrency from "../../../../utils/formatCurrency";
-import { IChoiceDataWithId } from "./multiple-select.pages.templateOne.components";
-import { StyledOptionsWrapper, StyledOptionsTitleContainer, StyledOptionsListContainer, StyledOptionsList, StyledOptionsListItem, StyledOptionsRadioButton, StyledOptionsRadioButtonContainer } from "./options-list.menu.pages.templateOne.components";
+import { useAppSelector } from '../../../../redux/hooks.redux';
+import { selectLanguage, selectLanguageCode } from '../../../../redux/slices/configuration.slices.redux';
+import { amplitudeEvent, constructEventName } from '../../../../utils/amplitude.util';
+import formatCurrency from '../../../../utils/formatCurrency';
+import CustomLink from '../../common/amplitude/customLink';
+import { IChoiceDataWithId } from './multiple-select.pages.templateOne.components';
+import {
+  StyledOptionsWrapper,
+  StyledOptionsTitleContainer,
+  StyledOptionsListContainer,
+  StyledOptionsList,
+  StyledOptionsListItem,
+  StyledOptionsRadioButton,
+  StyledOptionsRadioButtonContainer,
+} from './options-list.menu.pages.templateOne.components';
 
 export interface IPropsMenuPageCategoryListItem {
-  selectedOption: number|undefined
-  choice: IChoiceDataWithId
-  selectionMultipleId: number
-  setSelectionMultipleId(id: number): void
-  getNextIndex(): number
-  setSelectedOption(name: number|undefined): void
+  selectedOption: number | undefined;
+  choice: IChoiceDataWithId;
+  selectionMultipleId: number;
+  setSelectionMultipleId(id: number): void;
+  getNextIndex(): number;
+  setSelectedOption(name: number | undefined): void;
 }
 
+const MenuPageMultipleSelector: FunctionComponent<IPropsMenuPageCategoryListItem> = ({
+  choice,
+  selectedOption,
+  selectionMultipleId,
+  setSelectionMultipleId,
+  getNextIndex,
+  setSelectedOption,
+}) => {
+  const language = useAppSelector(selectLanguage);
+  const languageCode = useAppSelector(selectLanguageCode);
+  const [optionKey] = useState(getNextIndex());
 
-const MenuPageMultipleSelector: FunctionComponent<IPropsMenuPageCategoryListItem> = ({ choice, selectedOption, selectionMultipleId, setSelectionMultipleId, getNextIndex, setSelectedOption }) => {
-  const language = useAppSelector(selectLanguage)
-  const languageCode = useAppSelector(selectLanguageCode)
-  const [ optionKey ] = useState(getNextIndex())
+  const isOptionOpen = selectedOption === optionKey;
+  const toggle = () => setSelectedOption(isOptionOpen ? optionKey + 1 : optionKey);
 
-  const isOptionOpen = selectedOption === optionKey
-  const toggle = () => setSelectedOption(isOptionOpen? optionKey+1: optionKey)
+  return choice.options && choice.options.length > 0 ? (
+    <StyledOptionsWrapper>
+      <CustomLink
+        amplitude={{
+          type: 'card',
+          text: choice.name_json[language],
+        }}
+        callback={toggle}
+        Override={StyledOptionsTitleContainer}
+      >
+        <p style={{ margin: 0, padding: 12 }}>{choice.name_json[language]}</p>
+        <p style={{ margin: 0, padding: 12 }}>
+          {isOptionOpen ? 'Choose One' : choice.options.filter((i) => i.id === selectionMultipleId)[0].name_json[language]}
+        </p>
+      </CustomLink>
 
+      <StyledOptionsListContainer isOptionOpen={isOptionOpen}>
+        <StyledOptionsList>
+          {choice.options.map((option, index) => (
+            <StyledOptionsListItem
+              key={option.name_json.english}
+              onClick={() => {
+                if (option.id !== selectionMultipleId) {
+                  setSelectionMultipleId(option.id);
+                }
+                setSelectedOption(optionKey + 1);
 
-  return choice.options && choice.options.length > 0? <StyledOptionsWrapper>
-    <StyledOptionsTitleContainer onClick={toggle}>
-      <p style={{ margin: 0, padding: 12 }}>{choice.name_json[language]}</p>
-      <p style={{ margin: 0, padding: 12 }}>{isOptionOpen? "Choose One": choice.options.filter(i => i.id === selectionMultipleId)[0].name_json[language]}</p>
-    </StyledOptionsTitleContainer>
-    <StyledOptionsListContainer isOptionOpen={isOptionOpen}>
-      <StyledOptionsList>
-        {choice.options.map(option => <StyledOptionsListItem key={option.name_json.english} onClick={() => {
-          if (option.id !== selectionMultipleId) {
-            setSelectionMultipleId(option.id)
-          }
-          setSelectedOption(optionKey+1)
-        }}>
-          <StyledOptionsRadioButtonContainer>
-            <StyledOptionsRadioButton multiselect={false} selected={option.id === selectionMultipleId} />
-            <span style={{ margin: 0, padding: 12 }}>{option.name_json[language]}</span>
-          </StyledOptionsRadioButtonContainer>
-          {option.price > 0 && <span style={{ margin: 0, padding: 12 }}>+{formatCurrency(option.price, languageCode)}</span>}
-        </StyledOptionsListItem>)}
-      </StyledOptionsList>
-    </StyledOptionsListContainer>
-  </StyledOptionsWrapper>: <></>
-}
+                amplitudeEvent(constructEventName(option.name_json[language], 'card'), {
+                  id: option.id,
+                  data: {
+                    product_index: index,
+                    price: option.price,
+                    name: option.name_json,
+                  },
+                });
+              }}
+            >
+              <StyledOptionsRadioButtonContainer>
+                <StyledOptionsRadioButton multiselect={false} selected={option.id === selectionMultipleId} />
+                <span style={{ margin: 0, padding: 12 }}>{option.name_json[language]}</span>
+              </StyledOptionsRadioButtonContainer>
+              {option.price > 0 && <span style={{ margin: 0, padding: 12 }}>+{formatCurrency(option.price, languageCode)}</span>}
+            </StyledOptionsListItem>
+          ))}
+        </StyledOptionsList>
+      </StyledOptionsListContainer>
+    </StyledOptionsWrapper>
+  ) : (
+    <></>
+  );
+};
 
-export default MenuPageMultipleSelector
+export default MenuPageMultipleSelector;
