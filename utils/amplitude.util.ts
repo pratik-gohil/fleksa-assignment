358c25a1a759df4ca8f6504b85619606c235a627
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-08-21 16:24:05 +0530

    try to fix error boundary

--- utils/amplitude.util.ts
+++ utils/amplitude.util.ts
@@ -3,19 +3,19 @@ import { AmplitudeEventNodes } from '../interfaces/common/amplitude.common.inter
 const AMPLITUDE_KEY = '13e94566ae299c8fe7697c783f2fba7b';
 
 export const initAmplitude = () => {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && window.amplitude) {
     window.amplitude.getInstance().init(AMPLITUDE_KEY);
   }
 };
 
 export const setAmplitudeUserId = (userId: string) => {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && window.amplitude) {
     window.amplitude.getInstance().setUserId(userId);
   }
 };
 
 export const amplitudeEvent = (name: string, params?: {}) => {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && window.amplitude) {
     window.amplitude.getInstance().logEvent(name, params);
   }
 };
