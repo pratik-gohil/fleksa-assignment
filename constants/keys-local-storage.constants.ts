7dbe89fee8848dba987c356f92cda1b5ee4c0f97
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-09-25 23:25:30 +0530

    added: state subscription to local on user info

--- constants/keys-local-storage.constants.ts
+++ constants/keys-local-storage.constants.ts
@@ -1,3 +1,4 @@
-export const LS_CART = "@CART"
-export const LS_CHECKOUT = "@CHECKOUT"
-export const LS_GUEST_USER_ADDRESS = "@LS_GUEST_USER_ADDRESS"
+export const LS_CART = '@CART';
+export const LS_CUSTOMER_INFO = '@CUSTOMER_INFO';
+export const LS_CHECKOUT = '@CHECKOUT';
+export const LS_GUEST_USER_ADDRESS = '@GUEST_USER_ADDRESS';
--- redux/slices/user.slices.redux.ts
+++ redux/slices/user.slices.redux.ts
@@ -1,7 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { HYDRATE } from 'next-redux-wrapper';
 import { AddressTypes } from '../../components/templateOne/common/addresses/address-manager.common.templateOne.components';
-import { ICustomer } from '../../interfaces/common/customer.common.interfaces';
+import {
+  ICustomer,
+  IParticularAddress,
+  IParticularOrder,
+  IParticularOrderBriefInfo,
+} from '../../interfaces/common/customer.common.interfaces';
 import { RootState } from '../store.redux';
 
 const SLICE_NAME = 'user';
@@ -9,25 +14,30 @@ const SLICE_NAME = 'user';
 export interface IUserSliceState {
   bearerToken: string | null;
   customer: ICustomer;
+  orders?: Array<IParticularOrder>;
+  current_order?: IParticularOrderBriefInfo;
+  all_address: Array<IParticularAddress>;
 }
 
-const initialState: IUserSliceState = {
+export const initalCustomerData = {
+  name: '',
+  email: '',
+  country_code: '49',
+  phone: '',
+  email_verified: 0,
+  phone_verified: 0,
+};
+
+export const userInitialState: IUserSliceState = {
   bearerToken: null,
-  customer: {
-    name: '',
-    email: '',
-    country_code: '49',
-    phone: '',
-    email_verified: 0,
-    phone_verified: 0,
-    all_address: [],
-    orders: [],
-  },
+  customer: initalCustomerData,
+  all_address: [],
+  orders: [],
 };
 
 export const UserSlice = createSlice({
   name: SLICE_NAME,
-  initialState,
+  initialState: userInitialState,
   reducers: {
     updateBearerToken(state, action) {
       state.bearerToken = action.payload;
@@ -51,32 +61,32 @@ export const UserSlice = createSlice({
       state.customer.email_verified = action.payload;
     },
     updateLoadAddressesList(state, action) {
-      state.customer.all_address = action.payload;
+      state.all_address = action.payload;
     },
     updateCustomerOrderHistory(state, action) {
-      state.customer.orders = action.payload;
+      state.orders = action.payload;
     },
     updateNewCustomerAddress(state, action) {
-      state.customer.all_address?.push(action.payload);
+      state.all_address?.push(action.payload);
     },
     updateExistCustomerAddress(state, action) {
-      const index = state.customer.all_address?.findIndex((address) => action.payload.id === address.id);
+      const index = state.all_address?.findIndex((address) => action.payload.id === address.id);
 
       if (index !== -1) {
-        state.customer.all_address[index] = action.payload;
+        state.all_address[index] = action.payload;
       }
     },
     updateExistCustomerAddressOrAddNew(state, action) {
-      const index = state.customer.all_address?.findIndex((address) => action.payload.id === address.id);
+      const index = state.all_address?.findIndex((address) => action.payload.id === address.id);
 
       if (index !== -1) {
-        state.customer.all_address[index] = action.payload;
+        state.all_address[index] = action.payload;
       } else {
-        state.customer.all_address?.push(action.payload);
+        state.all_address?.push(action.payload);
       }
     },
     deleteCustomerAddress(state, action) {
-      state.customer.all_address = state.customer.all_address.filter((address) => address.id !== action.payload);
+      state.all_address = state.all_address.filter((address) => address.id !== action.payload);
     },
   },
   extraReducers: {
@@ -108,8 +118,8 @@ export const {
 export const selectIsUserLoggedIn = (state: RootState) => !!state.user.bearerToken;
 export const selectBearerToken = (state: RootState) => state.user.bearerToken;
 export const selectCustomer = (state: RootState) => state.user.customer;
-export const selectCustomerOrderHistory = (state: RootState) => state.user.customer.orders;
+export const selectCustomerOrderHistory = (state: RootState) => state.user.orders;
 export const selectAddressByType = (state: RootState, addressType: AddressTypes) =>
-  state.user.customer.all_address?.find((i) => i.address_type === addressType);
+  state.user.all_address?.find((i) => i.address_type === addressType);
 export const selectAddressById = (state: RootState, id: number | null) =>
-  id ? state.user.customer.all_address?.find((i) => i.id === id) : undefined;
+  id ? state.user.all_address?.find((i) => i.id === id) : undefined;
