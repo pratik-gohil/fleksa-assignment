427d9a3b3371ad43cced34afd5633dce5db384ad
Pratik Gohil <pr.tikgohil@gmail.com>
2021-10-02 19:53:08 +0530

    added: app buttons oreder placed page

--- templates/one/order-placed.one.templates.tsx
+++ templates/one/order-placed.one.templates.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { Col, Container, Row } from 'react-grid-system';
 import styled from 'styled-components';
+import AppButtons from '../../components/templateOne/common/appButtons/app-buttons.common.templateOne.components';
 import { BREAKPOINTS } from '../../constants/grid-system-configuration';
 import { useAppSelector } from '../../redux/hooks.redux';
 import { selectLanguageCode } from '../../redux/slices/configuration.slices.redux';
@@ -44,8 +45,8 @@ const Image = styled.img`
 `;
 
 const OrderPlacedPageTemplateOne: FunctionComponent = ({}) => {
-  const languageCode = useAppSelector(selectLanguageCode)
-  
+  const languageCode = useAppSelector(selectLanguageCode);
+
   return (
     <Wrapper>
       <Container>
@@ -54,6 +55,7 @@ const OrderPlacedPageTemplateOne: FunctionComponent = ({}) => {
             <Title>Your order is placed successfully</Title>
             <Image src={OrderPlacedImage} alt="person jumping" />
             <ViewManageButton href={`/${languageCode}/account/order-history`}>View or Manage Order</ViewManageButton>
+            <AppButtons direction="row" />
           </Col>
         </Row>
       </Container>
